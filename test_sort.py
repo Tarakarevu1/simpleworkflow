6a05e5bf7dd1f3a978b3aa52813d0d6747892fcd
Tarakarevu1 <taraka.revu@intel.com>
2025-01-27 11:38:48 +0530

    Update test_sort.py

--- test_sort.py
+++ test_sort.py
@@ -8,15 +8,12 @@ class TestBubbleSort(unittest.TestCase):
     def test_empty_array(self):
         self.assertEqual(bubble_sort([]), [])
     
-    def test_single_element_array(self):
-        self.assertEqual(bubble_sort([1]), [1])
-    
-    def test_already_sorted_array(self):
-        self.assertEqual(bubble_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
-    
-    def test_reverse_sorted_array(self):
-        self.assertEqual(bubble_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
-    
+    def test_sorted2(self):
+        self.assertEqual(bubble_sort([10,9,8,7,6,5]),[5,6,7,8,9,10])
+
+    def test_sorted_array(self):
+        self.assertEqual(bubble_sort([1, 2, 3, 7]), [1, 2, 3, 7])
+        
     def test_duplicate_elements_array(self):
         self.assertEqual(bubble_sort([3, 1, 2, 3, 1]), [1, 1, 2, 3, 3])
 
