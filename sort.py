ed810d1460296031884e90f36995b7f06125fd98
Tarakarevu1 <taraka.revu@intel.com>
2025-01-27 13:18:26 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,10 +1,9 @@
 def bubble_sort(arr):
-    print("Bubble_sort")
     arr_copy = arr[:]
     n = len(arr_copy)
     for i in range(n):
         for j in range(0,n-i-1):
             if arr_copy[j] > arr_copy[j + 1]:
-                # Swap the elements
                 arr_copy[j], arr_copy[j + 1] = arr_copy[j + 1], arr_copy[j]
+    print("Bubble_sort")
     return arr_copy
